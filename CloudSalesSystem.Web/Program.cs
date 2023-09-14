145d311f48ce068d5f7a8c2b5240f84551f505e1
Klepic95 <klepicpredrag@gmail.com>
2023-09-14 12:42:18 +0200

    adjusting program.cs file with the DI

--- CloudSalesSystem.Web/Program.cs
+++ CloudSalesSystem.Web/Program.cs
@@ -1,3 +1,12 @@
+using CloudSalesSystem.Business;
+using CloudSalesSystem.Business.Interfaces;
+using CloudSalesSystem.DAL;
+using CloudSalesSystem.DAL.Interfaces;
+using CloudSalesSystem.Proxy;
+using CloudSalesSystem.Proxy.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +15,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<CSSContext>(options =>
+{
+    options
+    .UseSqlServer(builder.Configuration.GetConnectionString("Default"))
+    .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+});
+
+// Add Services
+builder.Services.AddScoped<ICSSService, CSSService>();
+
+// Add Repositories
+builder.Services.AddScoped<ICSSRepository, CSSRepository>();
+
+// Add Proxies
+builder.Services.AddScoped<ICCPProxy, CCPProxy>();
 
 var app = builder.Build();
 
